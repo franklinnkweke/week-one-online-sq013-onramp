42a0b8605af9e4835180841520bd82bac1b7614a
franklinnkweke <franklinnkweke@gmail.com>
2022-09-13 23:49:28 +0100

    done

--- algorithms/challenge-1.js
+++ algorithms/challenge-1.js
@@ -1,4 +1,7 @@
 function secondLargest(arr) {
+        arr.sort(function(a, b){return a - b}); //sorts array in ascending
+        var index = arr.length - 2;
+        return arr[index];
+    }
   
-}
 module.exports = secondLargest
--- algorithms/challenge-2.js
+++ algorithms/challenge-2.js
@@ -1,5 +1,15 @@
 function filterArray(arr) {
  
+} var res = [];//empty array
+for (let i = 0; i < arr.length; i++) {
+    var value = arr[i];
+    var isNotaNumber = isNaN(value);
+  
+    if(!isNotaNumber){
+        res.push(value);
+    }
 }
+return res;
+
 
 module.exports = filterArray
