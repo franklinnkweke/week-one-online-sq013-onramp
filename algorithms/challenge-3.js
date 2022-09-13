d6aef98bf0b3cbdd269923d6f615bd032705bd9d
franklinnkweke <franklinnkweke@gmail.com>
2022-09-13 23:58:22 +0100

    done

--- algorithms/challenge-3.js
+++ algorithms/challenge-3.js
@@ -1,5 +1,16 @@
 function sumMix(arr) {
    
+ } var result = 0;
+ for(let i = 0; i < arr.length; i++){
+     var value = arr[i];
+     var type = typeof value;
+     
+     if(type != "number"){
+         value = parseInt(value);
+     }
+     
+     result = result + value;
  }
+ return result;
 
   module.exports = sumMix
--- algorithms/challenge-4.js
+++ algorithms/challenge-4.js
@@ -1,5 +1,8 @@
 function sumTwoSmallestNums(arr) {
-    
+    arr.sort(function(a, b){return a - b}); //sorts array in ascending
+    var a = arr[0];
+    var b = arr[1]
+    return a + b;
+}
  
- }
   module.exports =sumTwoSmallestNums
